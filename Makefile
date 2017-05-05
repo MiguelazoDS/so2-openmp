cd34d91cd8769700cf656498c20d23c4f2e214b8
Miguel <migue7490@gmail.com>
2017-05-05 16:35:07 -0300

    73 file created which contains complex signals

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ make:
 	gcc -Wall -Werror -pedantic -lm procedural.c -o procedural
 clean:
 	rm -rf procedural
+	rm -rf *.txt
--- procedural.c
+++ procedural.c
@@ -8,11 +8,14 @@
 int main(int argc, char const *argv[]) {
   char *nombre="pulsos.iq";
   FILE *file_in;
+  FILE *file_out;
   uint16_t samples;
   int ciclo;
   int count;
   int i;
   float *valores;
+  char auxiliar[10];
+  char valor[20];
   file_in=fopen(nombre,"rb");
   if (file_in==NULL){
    perror("No se puede abrir el archivo binario");
@@ -26,11 +29,17 @@ int main(int argc, char const *argv[]) {
     fread(valores, sizeof(float), ciclo, file_in);
     count++;
     ciclo--;
-    for (i = 0; i < ciclo/2; i+=2) {
-      printf("valor 1: %.10f, valor 2: %.10f resultado: %.10f\n",*(valores+i),*(valores+i+1), sqrt(pow(*(valores+i),2)+pow(*(valores+i+1),2)));
+    sprintf(auxiliar,"%d.txt",count);
+    file_out=fopen(auxiliar,"a");
+    sprintf(valor,"%d\n",samples);
+    fputs(valor,file_out);
+    for (i = 0; i < ciclo; i+=2) {
+      sprintf(valor,"%.10f\n",sqrt(pow(*(valores+i),2)+pow(*(valores+i+1),2)));
+      /*printf("valor 1: %.10f, valor 2: %.10f resultado: %.10f\n",*(valores+i),*(valores+i+1), sqrt(pow(*(valores+i),2)+pow(*(valores+i+1),2)));*/
+      fputs(valor,file_out);
     }
     printf("%d\n", count);
-
+    fclose(file_out);
   }
   return 0;
 }
