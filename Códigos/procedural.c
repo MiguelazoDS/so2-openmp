40688fe59c950f351b25d4e401c67f46cac04e94
Miguel <migue7490@gmail.com>
2017-05-31 11:04:02 -0300

    csv implementation deleted

--- Códigos/procedural.c
+++ Códigos/procedural.c
@@ -45,15 +45,9 @@ int cantidad_pulsos(FILE **file_in){
 }
 
 void matrices(FILE **file_in,float ***matrix_v,float ***matrix_h){
-  FILE *file_V;
-  FILE *file_H;
   int i,j,ciclo;
   char *nombre="pulsos.iq";
-  char *canal_v="canalV";
-  char *canal_h="canalH";
   float *valores;
-  /*char *lineas_v=malloc(100*sizeof(float));
-  char *lineas_h=malloc(100*sizeof(float));*/
   int muestras_gate,cant=0;
   float acumulador_v;
   float acumulador_h;
@@ -65,9 +59,6 @@ void matrices(FILE **file_in,float ***matrix_v,float ***matrix_h){
 
   *file_in=fopen(nombre,"rb");
   while(fread(&samples,1,sizeof(uint16_t), *file_in)){
-  	file_V=fopen(canal_v,"w");
-  	file_H=fopen(canal_h,"w");
-
     /*La cantidad total de muestras de cada pulso es 4 veces el valor samples.*/
     ciclo=4*samples;
     /*Reservo la cantidad necesaria para guardar un pulso.*/
@@ -80,30 +71,20 @@ void matrices(FILE **file_in,float ***matrix_v,float ***matrix_h){
     for (i = 0; i < ciclo; i+=2){
       /*Guarda los valores complejos del canal V en un archivo y los del canal H en otro archivo.*/
       if(j>=0&&j<samples){
-				fprintf(file_V, "%.10f\n", sqrt(pow(*(valores+i),2)+pow(*(valores+i+1),2)));
 				cv[j]=sqrt(pow(*(valores+i),2)+pow(*(valores+i+1),2));
       }
       if(j>=samples){
-				fprintf(file_H, "%.10f\n", sqrt(pow(*(valores+i),2)+pow(*(valores+i+1),2)));
 				ch[j-samples]=sqrt(pow(*(valores+i),2)+pow(*(valores+i+1),2));
       }
       j++;
     }
 
-		/*for (i = 0; i < samples; i++) {
-			printf("canal v: %.10f canal h:%.10f\n", cv[i],ch[i]);
-		}*/
-    fclose(file_V);
-    fclose(file_H);
-
-    file_V=fopen(canal_v,"r");
-    file_H=fopen(canal_h,"r");
     acumulador_v=0;
     acumulador_h=0;
     muestras_gate=0;
     aux=samples;
-
     cant=0;
+		
 		for (i = 0; i < samples; i++) {
 			acumulador_v+=cv[i];
 			acumulador_h+=ch[i];
@@ -119,24 +100,6 @@ void matrices(FILE **file_in,float ***matrix_v,float ***matrix_h){
 				}
 			}
 		}
-    /*while(samples){
-      samples--;
-      fgets(lineas_v, 100, file_V);
-      fgets(lineas_h, 100, file_H);
-      acumulador_v+=atof(lineas_v);
-      acumulador_h+=atof(lineas_h);
-      muestras_gate++;
-      if(muestras_gate==(int)(aux*2e-3)){
-        if(cant<GATES){
-					(*matrix_v)[filas][cant]=acumulador_v/(int)(aux*2e-3);
-					(*matrix_h)[filas][cant]=acumulador_h/(int)(aux*2e-3);
-          cant++;
-          acumulador_v=0;
-          acumulador_h=0;
-          muestras_gate=0;
-        }
-      }
-    }*/
 		filas++;
     /*Libera la memoria para poder ser alocar memoria nuevamente.*/
     free(valores);
