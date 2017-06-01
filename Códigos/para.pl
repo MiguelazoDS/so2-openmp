8af64b5c892712a1a07f5c297f2e621db1121662
Miguel <migue7490@gmail.com>
2017-06-01 20:24:47 -0300

    para.pl script added to plot parallel executions

---
+++ Códigos/para.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use Chart::Gnuplot;
+
+my $ejecuciones=1;
+my @tiempos;
+my @hilos;
+
+for $var(1..10)
+{
+  for $a(1..$ejecuciones){
+     $temp=`./paralelo $var`;
+    if($temp =~ /total:\s+(\d+.\d+)/)
+    {
+      $acumulador += $1;
+    }
+  }
+  printf "Tiempo promedio tomando $ejecuciones ejecuciones con $var hilos: %.5f\n",$acumulador/$ejecuciones;
+  push @tiempos, $acumulador/$ejecuciones;
+  push @hilos, $var;
+  $acumulador=0;
+}
+
+my $chart = Chart::Gnuplot->new(
+  output => "grafico.jpg",
+);
+
+my $dataset = Chart::Gnuplot::DataSet->new(
+  xdata => \@hilos,
+  ydata => \@tiempos,
+  style => "line",
+  title => "hilos vs tiempo",
+);
+
+$chart -> plot2d($dataset);
